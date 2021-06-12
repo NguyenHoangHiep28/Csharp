d31798f060fd4e4f16c78c1248315ada1c9bf820
Hiep Nguyen <71130020+NguyenHoangHiep28@users.noreply.github.com>
2021-06-12 21:29:51 +0700

    Create ASM8

---
+++ ASM8/Exercise1/Member.cs
@@ -0,0 +1,40 @@
+﻿namespace Exercise1
+{
+    class Member
+    {
+        private string job;
+        private int registrationFee;
+
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+        public void SetJob(string jobName)
+        {
+            job = jobName;
+        }
+        public string GetJob()
+        {
+            return job;
+        }
+        public int GetFee()
+        {
+            switch (job)
+            {
+                case "Student":
+                    registrationFee = 1000;
+                    break;
+                case "Teacher":
+                    registrationFee = 1400;
+                    break;
+                case "Retired":
+                    registrationFee = 1500;
+                    break;
+                case "Others":
+                    registrationFee = 2000;
+                    break;
+            }
+            return registrationFee;
+        }
+    }
+}
---
+++ ASM8/Exercise2/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Novel novel = new Novel(2);
+            novel[0] = new Book("100 - 123 - 456", "Harry Potter", 101);
+            novel[1] = new Book("110 - 133 - 567", "Sherlock Holmes", 201);
+            Console.WriteLine("CATEGORY: NOVEL");
+            novel.DisplayInfor();
+
+            Console.ReadKey();
+        }
+    }
+}
