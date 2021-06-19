9cb9029a63359cbc754f9070f0b3a634ae5ec7af
Hiep Nguyen <71130020+NguyenHoangHiep28@users.noreply.github.com>
2021-06-19 18:38:58 +0700

    Create AP Practical Exam

---
+++ APPracticalExam/Exercise1/People.cs
@@ -0,0 +1,19 @@
+﻿namespace APPracticalExam
+{
+    class People
+    {
+        public string Name { get; set; }
+        public bool Gender { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            string gender;
+            if (Gender)
+                gender = "Male";
+            else
+                gender = "Female";
+            return $"Name = {Name}, Gender = {gender}, Age = {Age}";
+        }
+    }
+}
---
+++ APPracticalExam/Exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace APPracticalExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People people = new People() { Name = "Harry", Gender = true, Age = 19 };
+            Console.WriteLine("Person details: " + people.ToString());
+            people.Age += 10;
+            Console.WriteLine("Person details (after incrementing age): " + people.ToString());
+            Console.WriteLine("---------------------------------------------------");
+            People another = new People() { Name = "Jose", Gender = false, Age = 20 };
+            Console.WriteLine("Person details: " + another.ToString());
+            another.Age += 5;
+            Console.WriteLine("Person details (after incrementing age): " + another.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
